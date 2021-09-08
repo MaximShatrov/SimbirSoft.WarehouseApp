5100656eaffb308c9d12cb09b0f3868053187319
Maxim <MaximShatrov@users.noreply.github.com>
2021-09-08 23:11:16 +0400

    Создание проекта в spring initializr со всеми зависимостями из ТЗ

---
+++ src/main/java/com/simbirsoft/shatrov/WarehouseApp/WarehouseAppApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.shatrov.WarehouseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseAppApplication.class, args);
+    }
+
+}
